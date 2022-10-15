5cb9a2b00f137b4676f5bed6141f7d7a1ce6b086
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-16 02:01:06 +0800

    Updated login form

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,19 +1,19 @@
 <template>
-   <v-app id="inspire">
+   <v-app id="login">
       <v-main>
          <v-container fluid fill-height>
             <v-layout align-center justify-center>
                <v-flex xs12 sm8 md4>
                   <v-card class="elevation-12">
                      <v-toolbar dark color="primary">
-                        <v-toolbar-title>Authorized Users:</v-toolbar-title>
+                        <v-toolbar-title>Login:</v-toolbar-title>
                      </v-toolbar>
                      <v-card-text>
                         <v-form>
                            <v-text-field
-                              v-model="username"
+                              v-model="email"
                               prepend-icon="mdi-account"
-                              name="username"
+                              name="email"
                               label="Login"
                               type="text"
                            ></v-text-field>
@@ -26,10 +26,17 @@
                               type="password"
                            ></v-text-field>
                         </v-form>
+                        <v-alert
+                           v-if="this.is_error"
+                           class="justify-center"
+                           dense
+                           text
+                           type="error"
+                        >{{ this.error_message }}</v-alert>
                      </v-card-text>
                      <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="primary" @click="login">Login</v-btn>
+                        <v-btn color="primary" @click="user_login()">Login</v-btn>
                      </v-card-actions>
                   </v-card>
                </v-flex>
@@ -40,6 +47,8 @@
  </template>
  
  <script>  
+import log_user_api from '../api/users/auth_user'
+
     export default {
         name: 'LoginForm',
         props: {
@@ -48,23 +57,31 @@
         
         data () {
             return {
-                username: '',
-                password: ''
+                email: '',
+                password: '',
+                is_error: null,
+                error_message: null
             }
         },
         methods: {
-            login() {
-               //sample login only
-               if(this.username == "test" && this.password == "1234"){
-                  this.$router.push('dashboard')
-               } else {
-                  console.log('Wrong password')
-               }
+            async user_login() {
                
+               const credentials = {
+                  "email": this.email,
+                  "password": this.password
+               }
+
+               const api_response = await log_user_api.login(credentials)
+
+               if (api_response.status === 1) {
+                  this.is_error = false
+                  this.$router.push('dashboard')
+               } else if (api_response.status === 0){
+                  this.is_error = true
+                  this.error_message = api_response.outputData.response.data.message
+               }
             }
         }
-
-        
     };
  </script>
  
