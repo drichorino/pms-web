df62cf5ad4f84f2129d644b2400d63f7fb0ede22
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-16 02:01:21 +0800

    Improved app CSS and error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,45 +1,31 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <div class="NotFound">
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card height="400" color="transparent" flat>
+            <div class="display-3 mt-15 mb-3 text-center">Page not found.</div>
+            <div class="teal lighten-3 white--text pa-1 text-center secondary rounded-lg">
+              The page you are trying to get to never existed in this reality,
+              or has migrated to a parallel universe.
+            </div>
+            <div class="paragraph-text text-center">
+              <br />
+              <br />Try going back to the
+              <NuxtLink to="/">
+                home page
+              </NuxtLink>. Or,
+              contact the administrators for support.
+            </div>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'EmptyLayout',
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
+<style>
+  .paragraph-text {
+    font-size: 18px;
   }
-}
-</script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
 </style>
