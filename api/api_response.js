ea5d2da9943665ed854f165c4ea669cce90ce6d9
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-16 02:00:22 +0800

    Initialized base API classes

---
+++ api/api_response.js
@@ -0,0 +1,13 @@
+export const success = (data) => {
+    return {
+        status: 1,
+        outputData: data
+    }
+}
+
+export const fail = (err) => {
+    return {
+        status: 0,
+        outputData: err
+    }
+}
---
+++ api/base.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import config from '../nuxt.config.js'
+
+axios.defaults.withCredentials = true
+
+class REQUEST {
+
+    static POST = async (params) => {
+        let promise = await axios.post(`${config.host.host}/${params.slug}`, params.payload)
+                            .then(
+                                (data) => params.success(data)
+                                )
+                            .catch(
+                                (err) => params.fail(err)
+                                )
+                 
+        return promise
+    }
+
+    static GET = async (params) => {
+        console.log(`${config.host.host}/${params.slug}`)
+        let promise = await axios.get(`${config.host.host}/${params.slug}`, params.payload)
+                            .then(
+                                (data) => console.log('get success')
+                            )
+                            .catch(
+                                (err) => console.log('get fail')
+                                )
+        return promise
+     }
+}
+
+export default REQUEST;
